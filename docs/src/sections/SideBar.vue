256d65ac2b626625847281b32ce445cf7cb0edf3
Ahmed Abd Elaziz <ahmed.abdelaziz760@gmail.com>
2021-06-09 21:27:16 +0200

    Add files via upload

--- docs/src/sections/SideBar.vue
+++ docs/src/sections/SideBar.vue
@@ -32,7 +32,7 @@ export default {
         {
           title: 'Documentation',
           pages: [
-            { to: '/props', title: 'Props' },
+            { to: '/props', title: 'Configuration options' },
             { to: '/events', title: 'Events' },
             { to: '/solts', title: 'Solts' },
             { to: '/demo', title: 'Basic Demo' },
